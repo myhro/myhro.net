0b4792904df845c822cb16dc23923ce8343b725f
Tiago Ilieve <tiago.myhro@gmail.com>
2014-02-21 14:20:29 -0300

    Configurados arquivos estaticos

--- myhronet/settings.py
+++ myhronet/settings.py
@@ -79,4 +79,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
+PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'static'),
+)
