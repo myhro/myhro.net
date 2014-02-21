eeff7f0fa0a9de0643209e0def1ff21aa022efa7
Tiago Ilieve <tiago.myhro@gmail.com>
2014-02-21 14:01:38 -0300

    Setando localizacao no settings

--- myhronet/settings.py
+++ myhronet/settings.py
@@ -65,9 +65,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
