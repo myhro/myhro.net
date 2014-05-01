bd57e2ae97c601a936fd6cd84cc85ff59ce16a13
Tiago Ilieve <tiago.myhro@gmail.com>
2014-05-01 17:01:48 -0300

    Adicionados testes de Views

--- myhronet/tests.py
+++ myhronet/tests.py
@@ -43,3 +43,47 @@ class URLFormTestCase(test.TestCase):
         if form.is_valid():
             form.save()
         self.assertEqual(URL.objects.count(), num_urls + 1)
+
+
+class ViewsTestCase(test.TestCase):
+    def setUp(self):
+        self.client = test.client.Client()
+        self.post_valid_url = {'longurl': 'http://google.com'}
+
+    def test_invalid_longurl(self):
+        response = self.client.post('/', {'longurl': 'not.a.valid/url'})
+        self.assertEqual(response.status_code, 200)
+        self.assertFormError(response, 'form', 'longurl', u'URL inválida.')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_invalid_shorturl(self):
+        response = self.client.get('/dontexist')
+        self.assertEqual(response.status_code, 404)
+
+    def test_same_url_twice(self):
+        num_urls = URL.objects.count()
+        # Salvando
+        response = self.client.post('/', self.post_valid_url)
+        self.assertEqual(URL.objects.count(), num_urls + 1)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'done.html')
+        # Carregando
+        response = self.client.post('/', self.post_valid_url)
+        # Não deve salvar uma nova URL na segunda tentativa.
+        self.assertEqual(URL.objects.count(), num_urls + 1)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'done.html')
+
+    def test_valid_shorturl(self):
+        response = self.client.post('/', self.post_valid_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'done.html')
+        response = self.client.get('/1')
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, self.post_valid_url['longurl'])
+
+    def test_valid_longurl(self):
+        response = self.client.post('/', self.post_valid_url)
+        self.assertContains(response, self.post_valid_url['longurl'])
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'done.html')
