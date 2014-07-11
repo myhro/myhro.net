6016611d4f67e3f34c4edda7900f2446f689f442
Tiago Ilieve <tiago.myhro@gmail.com>
2014-07-11 13:32:53 -0300

    Add dot to form errors

--- myhronet/forms.py
+++ myhronet/forms.py
@@ -15,16 +15,16 @@ class URLForm(forms.ModelForm):
             g = geoip.GeoIP(settings.GEOIP_PATH)
             country = g.country_code(cleaned_data['ip'])
             if country and not Country.objects.filter(country_code=country):
-                raise forms.ValidationError(_('Forbidden Country'))
+                raise forms.ValidationError(_('Forbidden Country.'))
         return cleaned_data['ip']
 
     def clean_longurl(self, *args, **kwargs):
         cleaned_data = super(URLForm, self).clean(*args, **kwargs)
         if not re.match('^https?://.*\.[a-z]{2,}', cleaned_data['longurl']):
-            raise forms.ValidationError(_('Invalid URL'))
+            raise forms.ValidationError(_('Invalid URL.'))
         for i in Blacklist.objects.all():
             if i.domain in cleaned_data['longurl']:
-                raise forms.ValidationError(_('Forbidden URL'))
+                raise forms.ValidationError(_('Forbidden URL.'))
         return cleaned_data['longurl']
 
     class Meta:
