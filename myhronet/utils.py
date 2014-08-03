4612d1ab6b3694e5926f54a55fefa305cac4e8bf
Tiago Ilieve <tiago.myhro@gmail.com>
2014-08-03 10:36:41 -0300

    Fix client IP detection behind reverse proxy
    The `X-Forwarded-For` header can be easily spoofed by a client. When
    forwarding a connection, nginx appends the real detected IP to the the
    list. So, what we need is actually the last one on this list, not the
    first one.

--- myhronet/utils.py
+++ myhronet/utils.py
@@ -4,6 +4,6 @@
 def get_client_ip(request):
     ip = request.META.get('HTTP_X_FORWARDED_FOR')
     if ip:
-        return ip.split(', ')[0]
+        return ip.split(', ')[-1]
     else:
         return request.META['REMOTE_ADDR']
